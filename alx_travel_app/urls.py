13744f6c7560e0d10aa848f82600f40027812424
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-06-15 17:24:25 +0300

    update swagger configurations

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg.views import get_swagger_view
+from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-swagger_view = get_swagger_view(
+schema_view = get_schema_view(
     openapi.Info(
         title="ALX Travel App API",
         default_version='v1',
@@ -33,5 +33,5 @@ swagger_view = get_swagger_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('listings.urls')),
-    path('swagger/', swagger_view, name='swagger'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
